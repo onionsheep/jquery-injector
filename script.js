2c561f9f106fe30bd6b6c5db2f64dcaec14d5f2d
Giorgos Giannoutsos <contact@nuc.gr>
2011-09-28 00:50:46 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var head = document.getElementsByTagName("head")[0];
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
+head.appendChild(script);
