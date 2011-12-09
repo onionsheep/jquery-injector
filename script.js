af44d9b245a6c8319592b974f01f5eca8e613ad0
Giorgos Giannoutsos <contact@nuc.gr>
2011-12-09 13:31:03 +0100

    replaced 1.7.0 with 1.7, in order to get teh newest version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var head = document.getElementsByTagName("head")[0];
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js";
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js";
 head.appendChild(script);
