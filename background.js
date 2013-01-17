a3d8b5046f1364902202f4a82716fa3e07e0db53
Giorgos Giannoutsos <contact@nuc.gr>
2013-01-17 23:57:51 +0100

    removed script.js and made it work

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.executeScript(null, { file: "script.js" }, function() {
-      var version = localStorage["jquery_version"] || "1.9";
-      chrome.tabs.sendMessage(tab.id, {version: version}, function(response) {
-        chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
-        chrome.browserAction.setBadgeText({text:version});
-        chrome.alarms.create('showBadgeText', {when: Date.now() + 3000});
-        chrome.alarms.onAlarm.addListener(function() {
-          chrome.browserAction.setBadgeText({text:""});
-        });
-      });
-    });
+var buttonClicked = function() {
+  var version = localStorage["jquery_version"] || "1.9";
+  var script = [];
+  script.push('var head = document.getElementsByTagName("head")[0];');
+  script.push('var script = document.createElement("script");');
+  script.push('script.type = "text/javascript";');
+  script.push('script.src = "https://ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.min.js";');
+  script.push('head.appendChild(script);');
+  chrome.tabs.executeScript(null, { code: script.join('') });
+  chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
+  chrome.browserAction.setBadgeText({text:version});
+  chrome.alarms.create('showBadgeText', {when: Date.now() + 3000});
+  chrome.alarms.onAlarm.addListener(function() {
+    chrome.browserAction.setBadgeText({text:""});
   });
-});
-
+};
+chrome.browserAction.onClicked.addListener(buttonClicked);
--- script.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    var version = request.version;
-    var head = document.getElementsByTagName("head")[0];
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "//ajax.googleapis.com/ajax/libs/jquery/" + version + "/jquery.min.js";
-    head.appendChild(script);
-   });
