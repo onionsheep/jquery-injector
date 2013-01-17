113996c93554ff68aacbb638e0e7ce68df62770b
Giorgos Giannoutsos <contact@nuc.gr>
2013-01-17 20:34:56 +0100

    v 0.2.3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var head = document.getElementsByTagName("head")[0];
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js";
+script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js";
 head.appendChild(script);
